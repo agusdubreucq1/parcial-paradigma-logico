84de5584e585f75cc00d855e03a1a90ada8dbdb7
Brunosaurio <bruno_deangelis@outlook.com>
2022-08-31 14:51:00 -0300

    punto 1

--- src/parcial.pl
+++ src/parcial.pl
@@ -1,6 +1,6 @@
 /*
-Nombre: Apellido, Nombre
-Legajo: 999999-9
+Nombre: Dubreucq, Agustin
+Legajo: 178247-2
 */
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -9,11 +9,48 @@ Legajo: 999999-9
 
 % Copiar base de conocimientos inicial aquí
 
+%obejtivo(Proyecto, Objetivo, TipoDeObjetivo)
+obejtivo(higiene, almejas, material(playa)).
+obejtivo(higiene, algas, material(mar)).
+obejtivo(higiene, grasa, material(animales)).
+obejtivo(higiene, hidroxidoDeCalcio, quimica([hacerPolvo, diluirEnAgua])).
+obejtivo(higiene, hidroxidodeSodio, quimica([mezclarIngredientes])).
+obejtivo(higiene, jabon, quimica([mezclarIngredientes, dejarSecar])).
+
+%prerrequisito(ObjetivoPrevio, ObjetivoSiguiente)
+prerrequisito(almejas, hidroxidoDeCalcio).
+prerrequisito(hidroxidoDeCalcio, hidroxidodeSodio).
+prerrequisito(algas, hidroxidodeSodio).
+prerrequisito(hidroxidodeSodio, jabon).
+prerrequisito(grasa, jabon).
+
+%conseguido(Objetivo)
+conseguido(almejas).
+conseguido(algas).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Punto 1
+sonDelReino(senku).
+sonDelReino(chrome).
+sonDelReino(kohaku).
+sonDelReino(suika).
 
-%% Implementar aquí...
+
+%puedeTrabajar(Persona, TipoDeObjetivo)
+
+puedeTrabajar(senku, quimica(_)).
+puedeTrabajar(chrome, material(Material)):-
+    Material \= animales.
+puedeTrabajar(kohaku, material(animales)).
+puedeTrabajar(chrome, quimica(ListaProcesos)):-
+    length(ListaProcesos, CantProcesos),
+    CantProcesos =< 3.
+puedeTrabajar(senku, artesania(Dificultad)):-
+    Dificultad =< 6.
+puedeTrabajar(Persona, artesania(Dificultad)):-
+    sonDelReino(Persona),
+    Dificultad < 3.
 
 %% Punto 2
 
